8c2c68b0a5247331ecef74239bb3ed70bf9b3ace
nyoung <kembel0116@gmail.com>
2025-03-10 13:22:24 +0900

    .

--- model.py
+++ model.py
@@ -1,1 +1,18 @@
-from tqdm import tqdm
+from transformers import AutoModelForVision2Seq, AutoProcessor
+from peft import LoraConfig, get_peft_model
+
+
+model_name = "openvla/openvla-7b"
+model = AutoModelForVision2Seq.from_pretrained(model_name)
+processor = AutoProcessor.from_pretrained(model_name)
+
+lora_config = LoraConfig(
+  r = 32,
+  lora_alpha = 16,
+  target_modules = ["q_proj", "v_proj"],
+  lora_dropout = 0.05,
+  bias = "none",
+  task_type = "SEQ_2_SEQ_LM"
+)
+
+model = get_peft_model(model, lora_config)
